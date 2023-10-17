f0aedb9b46809f5bcd5aaad7509fd3e7e2dd6c16
40gilad <40gilad.shenkar@gmail.com>
2023-10-17 14:42:27 +0300

    connection to database from chatbot script works well

--- Twilio/main.py
+++ Twilio/main.py
@@ -1,15 +1,15 @@
 import os
+import sys
+sys.path.append(os.path.abspath("C:\\Users\\40gil\\OneDrive\\Desktop\\Helpful\\HelpfulAI\\Database\\PythonDatabase"))
+import DBmain as Database
 from dotenv import load_dotenv
 from twilio.rest import Client
-
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse, Message
 import TwilioSession as ts
 
 app = Flask(__name__)
 session_manager = ts.SessionManager()
-
-
 
 def send_msg(_to,_body,_from="whatsapp:+14155238886"):
     message = client.messages.create(
@@ -41,66 +41,15 @@ def wa_reply():
 
 
 if __name__ == "__main__":
-
+    hdb = Database.Database()
+    #kaki=hdb.select_with("employee", personal_id="313416562")
+    for x in hdb.select_with("employee"):
+        print(x)
     load_dotenv(dotenv_path='Twilio.env')
     account_sid = os.getenv('SID')
     auth_token = os.getenv('TOKEN')
     phone_number = os.getenv('PHONE_NUMBER')
     client = Client(account_sid, auth_token)
     app.run(debug=True)
-    """
-    print("kaki")
-    s=ts.SessionManager()
-    s.create_session("12345")
-    s.create_session("54321")
-    s.inc_stage("12345")
-    d=s.get_session("12345")
-    e=s.get_session("54321")
-    print(d["stage"])
-    f=s.get_session("1111")
-    x=1
-    """
 
-
-"""
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse, Message
-import TwilioSession as ts
-
-app = Flask(__name__)
-session = ts.SessionManager()
-
-
-@app.route("/")
-def wa_reply():
-    msg_received = request.form.get('Body')
-    phone_number = request.headers.get('X-Twilio-Phone-Number')
-    # Check if the sender has a session
-
-    if not session.get_session(phone_number):
-        session.create_session(phone_number)
-
-    # Get the sender's session
-    session = session['session']
-
-    # Check if the sender's session is valid
-    if not session.is_valid():
-        # Create a new session for the sender
-        session = session['session'] = TwilioSession.Session('+15555555555', '1234567890abcdef')
-
-    # Handle the sender's message
-
-@app.route("/wa", methods=['POST'])
-def sms_reply():
-    # Fetch the message
-    print (request.form)
-    msg_recieved = request.form.get('Body')
-
-
-
-    resp = MessagingResponse()
-    resp.message(f"You said: {msg_recieved}")
-
-    return str(resp)
-"""
 
