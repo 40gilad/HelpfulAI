94e91167f52d80fce87a0b0985804928c0c389a5
40gilad <40gilad.shenkar@gmail.com>
2023-12-01 14:39:20 +0200

    minor changes in creation and added values to ease QA

--- Database/databse_creation.sql
+++ Database/databse_creation.sql
@@ -2,7 +2,7 @@ CREATE DATABASE helpful;
 USE helpful;
 CREATE TABLE person(
 			system_id int NOT NULL auto_increment,
-			person_id varchar(10),
+			person_id varchar(12),
 			person_name varchar(30) NOT NULL,
 			email varchar(255) NOT NULL,
 			phone varchar(10) NOT NULL,
@@ -32,24 +32,74 @@ CREATE TABLE employee(
     );
  
 CREATE TABLE customer(
-			system_id int,
+			system_id int primary key,
 			buisness_name varchar(50),
-			employee_id int NOT NULL,
-			backup_id int,
-			leader_id int NOT NULL,
-			status int NOT NULL,
+			status int NOT NULL DEFAULT 1,
 			
-			
-			primary key (system_id),
-			foreign key (employee_id) references employee(system_id),
-			foreign key (backup_id) references employee(system_id),
-			foreign key (leader_id) references employee(system_id)
+			foreign key (system_id) references person(system_id)
 
     );
    
 
 
 
+CREATE TABLE conversations(
+			conv_id VARCHAR(255) primary key,
+			conv_name VARCHAR(255) NOT NULL,
+			customer_id int NOT NULL,
+			employee_id int NOT NULL,
+			backup_id int,
+			leader_id int,
+			
+			foreign key (customer_id) references person (system_id),
+			foreign key (employee_id) references person (system_id),
+			foreign key (backup_id) references person (system_id),
+			foreign key (leader_id) references person (system_id)
+	);
+    
+
+
+
+CREATE TABLE messages (
+    			msg_id VARCHAR(255),
+    			conv_id VARCHAR(255),
+    			quoted_phone VARCHAR(20) NOT NULL,
+    			quoter_phone VARCHAR(20) NOT NULL,
+    			content TEXT,
+
+			primary key (msg_id,quoter_phone)
+
+);
+
+/*
+
+*/
+
+insert into _role values (1,"admin"),(2,"team_leader"),(3,"employee");
+
+insert into person (person_id,person_name,email,phone)
+	values("313416562","Gilad Meir","gilad@helpfulpro.biz","0526263862"),
+	("123456","Yonatan Meir","yona@gmail.com","0528449529");
+
+insert into employee (system_id,premission)
+	select
+	 (select system_id from person where person_id="313416562"),
+	(select role_id from _role where role_name="employee");
+
+insert into customer (system_id,buisness_name)
+	select
+	(select system_id from person where person_id="123456"),"yona's buisness";
+	
+insert into conversations (conv_id,conv_name,customer_id,employee_id)
+	select 
+	"120363198441376106@g.us","Yauu",
+	(select system_id from person where phone="0528449529"),
+	(select system_id from person where phone="0526263862");
+
+insert into messages(msg_id,conv_id,quoted_phone,quoter_phone,content)
+	values("1111","2222","987654","3210","manual insrtion from mysql for QA");
+
+/*
     
 CREATE TABLE board(
 			system_id int NOT NULL auto_increment,
@@ -69,5 +119,7 @@ CREATE TABLE employee_board(
             		foreign key(employee_id) references employee(system_id),
             		foreign key(board_id) references board (system_id)
 	);
+
+*/
     
-insert into _role values (1,"admin"),(2,"team_leader"),(3,"employee")
+
