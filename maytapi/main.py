098d17fe0d581ee3383f010778477b9e28d8479a
40gilad <40gilad.shenkar@gmail.com>
2024-01-02 17:33:06 +0200

    sending daily report to team leader works

--- maytapi/main.py
+++ maytapi/main.py
@@ -92,8 +92,8 @@ def get_next_question(raw_emp_phone):
 
 def finish_QnA(emp_phone, customer_phone):
     send_private_txt_msg(msg="תודה! הנה סיכום היום שלך:", to=[emp_phone])
-    #hdb.update_stage(system_id=hdb.get_system_id(phone_number=format_phone_for_selection(emp_phone)),stage=SESSION_DICT['SendMenu'])
-    send_daily_report(raw_emp_phone=emp_phone,raw_customer_phone=customer_phone)
+    # hdb.update_stage(system_id=hdb.get_system_id(phone_number=format_phone_for_selection(emp_phone)),stage=SESSION_DICT['SendMenu'])
+    send_daily_report(raw_emp_phone=emp_phone, raw_customer_phone=customer_phone)
 
 
 # --------------------------------------------------------------------------------------------------------------------#
@@ -331,10 +331,22 @@ def run_conversation(ses_stage, permission, raw_phone_number, income_msg, sys_id
             send_private_txt_msg(msg='בבקשה לענות רק בכן או לא', to=[raw_phone_number])
             return
         elif ses_stage == SESSION_DICT['ApproveQnA']:
-            is_approved = False
             if income_msg == "כן":
-                is_approved = True
-                send_daily_report(raw_emp_phone=raw_phone_number,)
+                send_private_txt_msg(msg="תודה! אל תשכחי להעביר את הסיכום ללקוח. אני כבר אשלח לכל השאר :)",
+                                     to=[raw_phone_number])
+                raw_customer_phone=hdb.get_customer_waiting_for_approve(
+                                      emp_phone=format_phone_for_selection(
+                                          raw_phone_number=raw_phone_number
+                                      ))
+                send_daily_report(raw_emp_phone=raw_phone_number,
+                                  raw_customer_phone=raw_customer_phone, is_approved=True)
+            elif income_msg== "לא":
+                send_private_txt_msg(msg="אוקיי. יש לתקן את הדוח ולשלוח אותו לראש הצוות וללקוח ", to=raw_phone_number)
+
+            hdb.delete_waiting_for_approve(customer_phone=raw_customer_phone)
+            hdb.update_stage(hdb.get_system_id(format_phone_for_selection(raw_phone_number=raw_phone_number)),
+                             stage=SESSION_DICT['SendMenu'])
+            return
         elif ses_stage == SESSION_DICT['QnA']:
             status = 0
             if income_msg == 'כן':
@@ -391,7 +403,7 @@ def start_QnA():
 
 def is_ready_for_QnA(emp_phone):
     send_private_txt_msg(f" היי *{hdb.get_employee_name(phone_number=format_phone_for_selection(emp_phone))}* \n"
-                         f" מוכנה לסיכום היום שלנו? ",to=[emp_phone])
+                         f" מוכנה לסיכום היום שלנו? ", to=[emp_phone])
 
 
 def send_next_QnA(raw_emp_phone):
@@ -403,20 +415,22 @@ def send_next_QnA(raw_emp_phone):
     # send_private_txt_msg(f"{question['BN']} asked you:\n {question['Q']}", raw_emp_phone)
 
 
-def send_daily_report(raw_emp_phone,raw_customer_phone, is_approved=False):
+def send_daily_report(raw_emp_phone, raw_customer_phone, is_approved=False):
     print(f"starting daily to number {raw_emp_phone}.is approved= {is_approved}")
     formatted_customer_phone = format_phone_for_selection(raw_phone_number=raw_customer_phone)
-    formatted_emp_phone=format_phone_for_selection(raw_phone_number=raw_emp_phone)
+    formatted_emp_phone = format_phone_for_selection(raw_phone_number=raw_emp_phone)
     for status in range(1, -1, -1):
         msgs = hdb.get_daily(customer_phone=formatted_customer_phone, status=status)
         if status == 0:
             txt = f"*זה ישאר למחר:*\n"
         elif status == 1:
             if is_approved:
-                txt = f"עבור העסק {hdb.get_buisness_name(formatted_customer_phone)} ביצענו היום : \n"
+                txt= f" * סיכום היום של {hdb.get_employee_name(phone_number=formatted_emp_phone)} * \n"
+                txt =txt+ f"עבור העסק {hdb.get_buisness_name(phone_number=formatted_customer_phone)} ביצענו היום : \n"
             elif not is_approved:
-                txt =f"* היי {hdb.get_buisness_name(formatted_customer_phone)} * \n הנה מה שעשיתי בשבילך היום:"
-                #txt = f"\n. זה מה שעשיתי בשבילך היום: {hdb.get_buisness_name(formatted_customer_phone)}* היי"
+                txt = f" * היי {hdb.get_buisness_name(phone_number=formatted_customer_phone)} *\n "
+                txt = txt + f"הנה מה שעשיתי בשבילך היום: \n"
+                # txt = f"\n. זה מה שעשיתי בשבילך היום: {hdb.get_buisness_name(formatted_customer_phone)}* היי"
             # txt = f"*Hi {hdb.get_buisness_name(raw_emp_phone)}!*\n here is what we did for you today:"
         counter = 1
         for m in msgs:
@@ -424,19 +438,20 @@ def send_daily_report(raw_emp_phone,raw_customer_phone, is_approved=False):
             counter = counter + 1
         if not is_approved:
             send_private_txt_msg(msg=txt, to=[raw_emp_phone])
-            hdb.update_stage(hdb.get_system_id(formatted_emp_phone),
-                             stage=SESSION_DICT['ApproveQnA'])
-            send_private_txt_msg(msg="האם את מאשרת את סיכום היום?", to=[raw_emp_phone])
         elif is_approved:
             send_private_txt_msg(msg=txt, to=[
-                                              hdb.get_employee_phone(
-                                                  system_id=hdb.get_team_leader_id(
-                                                      customer_id=hdb.get_system_id(
-                                                          phone_number=formatted_customer_phone
-                                                      )))])
-            hdb.update_stage(hdb.get_system_id(formatted_emp_phone),
-                             stage=SESSION_DICT['ApproveQnA'])
-            hdb.delete_daily(customer_phone=formatted_customer_phone)
+                format_phone_for_sending(phone_number=hdb.get_employee_phone(
+                    system_id=hdb.get_team_leader_id(
+                        customer_id=hdb.get_system_id(
+                            phone_number=formatted_customer_phone
+                        ))))])
+    if not is_approved:
+        hdb.update_stage(hdb.get_system_id(formatted_emp_phone),
+                         stage=SESSION_DICT['ApproveQnA'])
+        hdb.insert_waiting_for_approve(emp_number=formatted_emp_phone, customer_number=formatted_customer_phone)
+        send_private_txt_msg(msg="האם את מאשרת את סיכום היום?", to=[raw_emp_phone])
+    elif is_approved:
+        hdb.delete_daily(customer_phone=formatted_customer_phone)
 
 
 # endregion
